6b42cbd82bfef98517445660f51f47d4193dea54
egor <egorokt@inbox.ru>
2024-01-05 17:52:50 +0500

    добавлено движение чисел вверх, вниз, влево и вправо

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ class Game:
             [0, 0, 0, 0],
             [0, 0, 0, 0],
         ]
-        self.free_cells = [(y, x) for x in range(4) for y in range(4)]
         self.font = pygame.font.SysFont('arial', 150)
 
     def run(self):
@@ -29,9 +28,17 @@ class Game:
                 if event.type == pygame.QUIT:
                     exit()
                 if event.type == pygame.KEYDOWN:
-                    self.add_random_numbers()
-                    self.load_map()
-                    self.render_numbers_on_map()
+                    if event.key == pygame.K_DOWN:
+                        self.move_down()
+                    elif event.key == pygame.K_UP:
+                        self.move_up()
+                    elif event.key == pygame.K_RIGHT:
+                        self.move_right()
+                    elif event.key == pygame.K_LEFT:
+                        self.move_left()
+
+            self.load_map()
+            self.render_numbers_on_map()
             pygame.display.update()
             self.clock.tick(self.FPS)
 
@@ -55,10 +62,83 @@ class Game:
                     self.screen.blit(char, position)
 
     def add_random_numbers(self):
-        if len(self.free_cells) > 0:
-            index = random.randrange(len(self.free_cells))
-            cell = self.free_cells.pop(index)
+        free_cells = []
+        for y in range(4):
+            for x in range(4):
+                if self.map[y][x] == 0:
+                    free_cells.append((y, x))
+        if len(free_cells) > 0:
+            index = random.randrange(len(free_cells))
+            cell = free_cells.pop(index)
             self.map[cell[0]][cell[1]] = random.choice([2] * 10 + [4])
+
+    def move_left(self):
+        for row in self.map:
+            while 0 in row:
+                row.remove(0)
+            while len(row) < 4:
+                row.append(0)
+
+        for y in range(4):
+            for x in range(3):
+                if self.map[y][x] == self.map[y][x + 1] and self.map[y][x] != 0:
+                    self.map[y][x] *= 2
+                    del self.map[y][x + 1]
+                    self.map[y].append(0)
+
+        self.add_random_numbers()
+
+    def move_right(self):
+        for row in self.map:
+            while 0 in row:
+                row.remove(0)
+            while len(row) < 4:
+                row.insert(0, 0)
+
+        for y in range(4):
+            for x in range(3, 0, -1):
+                if self.map[y][x] == self.map[y][x - 1] and self.map[y][x] != 0:
+                    self.map[y][x] *= 2
+                    del self.map[y][x - 1]
+                    self.map[y].insert(0, 0)
+
+        self.add_random_numbers()
+
+    def move_up(self):
+        for x in range(4):
+            column = [self.map[y][x] for y in range(4)]
+            while 0 in column:
+                column.remove(0)
+            while len(column) < 4:
+                column.append(0)
+
+            for i in range(3):
+                if column[i] == column[i + 1] and column[i] != 0:
+                    column[i] *= 2
+                    del column[i + 1]
+                    column.append(0)
+            for y in range(4):
+                self.map[y][x] = column[y]
+
+        self.add_random_numbers()
+
+    def move_down(self):
+        for x in range(4):
+            column = [self.map[y][x] for y in range(4)]
+            while 0 in column:
+                column.remove(0)
+            while len(column) < 4:
+                column.insert(0, 0)
+
+            for i in range(3, 0, -1):
+                if column[i] == column[i - 1] and column[i] != 0:
+                    column[i] *= 2
+                    del column[i - 1]
+                    column.insert(0, 0)
+            for y in range(4):
+                self.map[y][x] = column[y]
+
+        self.add_random_numbers()
 
 
 if __name__ == '__main__':
